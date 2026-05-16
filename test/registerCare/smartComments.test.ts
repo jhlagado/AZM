7b0353684d72c2b442d35991abc9e677abcb09fa
John Hardy <jh@lagado.com>
2026-05-16 23:35:13 +1000

    test: cover located smart comment parsing

--- test/registerCare/smartComments.test.ts
+++ test/registerCare/smartComments.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { parseSmartCommentLine } from '../../src/registerCare/smartComments.js';
+import { parseSmartCommentLine, parseSmartComments } from '../../src/registerCare/smartComments.js';
 
 describe('register-care smart comments', () => {
   it('parses proc tags', () => {
@@ -52,5 +52,52 @@ describe('register-care smart comments', () => {
       carriers: ['carry'],
       name: 'flag',
     });
+  });
+
+  it('ignores unknown smart tags', () => {
+    expect(parseSmartCommentLine(';! @unknown {DE}')).toBeUndefined();
+  });
+
+  it('ignores malformed carrier payloads', () => {
+    expect(parseSmartCommentLine(';! @in DE raw')).toBeUndefined();
+    expect(parseSmartCommentLine(';! @in {} raw')).toBeUndefined();
+  });
+
+  it('parses located comments from module loader comment maps in deterministic order', () => {
+    const sourceLineComments = new Map<string, Map<number, string>>([
+      [
+        'src/zeta.asm',
+        new Map([
+          [20, '! @in {DE} raw'],
+          [1, 'ordinary comment'],
+        ]),
+      ],
+      [
+        'src/alpha.asm',
+        new Map([
+          [30, '! @in {HL} pointer'],
+          [10, '! @proc FOO'],
+          [20, 'ordinary comment'],
+        ]),
+      ],
+    ]);
+
+    expect(parseSmartComments(sourceLineComments)).toEqual([
+      {
+        file: 'src/alpha.asm',
+        line: 10,
+        comment: { kind: 'proc', name: 'FOO' },
+      },
+      {
+        file: 'src/alpha.asm',
+        line: 30,
+        comment: { kind: 'in', carriers: ['H', 'L'], name: 'pointer' },
+      },
+      {
+        file: 'src/zeta.asm',
+        line: 20,
+        comment: { kind: 'in', carriers: ['D', 'E'], name: 'raw' },
+      },
+    ]);
   });
 });
